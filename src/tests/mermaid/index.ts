590fbac91ce2c175ffa39d21fbeee32657d79aec
3y3 <3y3@ya.ru>
2025-10-17 00:18:14 +0300

    Fix tests

--- src/tests/mermaid/index.ts
+++ src/tests/mermaid/index.ts
@@ -6,63 +6,68 @@ const selectors = {
     mermaid_zoomin: 'div[data-action="zoomin"]',
 } as const;
 
-const png = {
-    mermaid_diagram: 'mermaid.png',
-    mermaid_contols: 'mermaid_controls.png',
-    mermaid_zoom: 'mermaid_zoom.png',
-} as const;
-
 test.describe('Mermaid', () => {
-    test.beforeEach(async ({page}) => {
-        await page.goto('./ru/mermaid/index');
-    });
-
     test.describe('Basic functionality', () => {
-        test('should render mermaid diagramm', async ({page}) => {
-            const mermaidDiagram = page.locator(selectors.mermaid_diagram);
-
-            await mermaidDiagram.screenshot({path: png.mermaid_diagram});
-            await expect(mermaidDiagram).toHaveScreenshot(png.mermaid_diagram);
+        test.beforeEach(async ({page}) => {
+            await page.goto('./ru/mermaid/');
         });
 
-        test('should render mermaid diagramm under safari browser', async () => {
-            const browser = await webkit.launch({headless: false});
-            const page = await browser.newPage();
-
-            await page.goto('./ru/mermaid/index');
-
+        test('should render diagram', async ({page}) => {
             const mermaidDiagram = page.locator(selectors.mermaid_diagram);
 
-            await mermaidDiagram.screenshot({path: 'safari_' + png.mermaid_diagram});
-            await expect(mermaidDiagram).toHaveScreenshot('safari_' + png.mermaid_diagram);
+            await expect(mermaidDiagram).toHaveScreenshot();
         });
 
-        test('should render diagramm with controls on click', async ({page}) => {
+        test('should render diagram with controls on click', async ({page}) => {
             const mermaidDiagram = page.locator(selectors.mermaid_diagram);
 
-            mermaidDiagram.click();
+            await mermaidDiagram.click();
 
             // Wait for controls animation
             await page.waitForTimeout(300);
 
-            await mermaidDiagram.screenshot({path: png.mermaid_contols});
-            await expect(mermaidDiagram).toHaveScreenshot(png.mermaid_contols);
+            await expect(mermaidDiagram).toHaveScreenshot();
         });
 
-        test('Mermaid zoom contros should work correctly', async ({page}) => {
+        test('should work correctly with scroll', async ({page}) => {
             const mermaidDiagram = page.locator(selectors.mermaid_diagram);
 
-            mermaidDiagram.click();
-            // Wait for controls animation
+            await mermaidDiagram.click();
+
             await page.waitForTimeout(300);
 
             await page.mouse.wheel(0, 600);
+            await page.waitForTimeout(300);
+
+            await expect(mermaidDiagram).toHaveScreenshot({maxDiffPixels: 5});
+        });
+
+        test('should work correctly with zoom controls', async ({page}) => {
+            const mermaidDiagram = page.locator(selectors.mermaid_diagram);
+
+            await mermaidDiagram.click();
+
+            await page.waitForTimeout(300);
+
             const zoomIn = page.locator(selectors.mermaid_zoomin);
+            await zoomIn.click({clickCount: 5, delay: 1000});
 
-            await zoomIn.click({clickCount: 5, delay: 100});
+            await page.waitForTimeout(300);
 
-            await mermaidDiagram.screenshot({path: png.mermaid_zoom, animations: 'disabled'});
-            await expect(mermaidDiagram).toHaveScreenshot(png.mermaid_zoom, {maxDiffPixels: 5});
+            await expect(mermaidDiagram).toHaveScreenshot({maxDiffPixels: 5});
+        });
+    });
+
+    test.describe('under Safari browser', () => {
+        test('should render diagram', async () => {
+            const browser = await webkit.launch();
+            const page = await browser.newPage();
+
+            await page.goto('./ru/mermaid/');
+
+            const mermaidDiagram = page.locator(selectors.mermaid_diagram);
+
+            await expect(mermaidDiagram).toHaveScreenshot();
         });
     });
 });
